b9445e78fc14b063d04fe6a432b8920738b567b5
Sreesha <sreesha.s.nath@gmail.com>
2020-09-13 22:43:18 -0600

    "creating new branch and adding a file"

--- script.sh
+++ script.sh
@@ -8,12 +8,12 @@ message="file committed"
 echo "Enter commit message"
 read message
 
-git commit -m "x$message"
+git commit -m "$message"
 git status
 
 branch = "master"
 echo "Enter branch"
-read $branch
+read branch
 git push origin $branch
 
 echo "!!!!!changes pushed to remote successfully!!!!!!"
