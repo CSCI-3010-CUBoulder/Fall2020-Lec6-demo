eac38fb7f6e6cd939a1fd339c17b528857d2d2b5
Sreesha <sreesha.s.nath@gmail.com>
2020-09-14 08:55:53 -0600

    printing based on number of parameters

--- ex2.cpp
+++ ex2.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
-
+//print command line arguments
 int main(int argc, char* argv[]) {
     std::cout << argv[0] << std::endl;
-    std::cout << argv[1] << std::endl;
+    if(argc > 1){
+        std::cout << argv[1] << std::endl;
+    }
 }
