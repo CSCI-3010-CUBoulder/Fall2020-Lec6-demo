83ff0627ef4d33a4721c0f3ebc17bc89153765a2
Sreesha <sreesha.s.nath@gmail.com>
2020-09-13 22:44:34 -0600

    "something"

--- script.sh
+++ script.sh
@@ -5,13 +5,14 @@ git add --all
 git status
 
 message="file committed"
+branch="master"
 echo "Enter commit message"
 read message
 
 git commit -m "$message"
 git status
 
-branch = "master"
+
 echo "Enter branch"
 read branch
 git push origin $branch
