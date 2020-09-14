20f385c1a1eec8bd8d300352dfa6172d85432112
SreeshaNath <31390313+SreeshaNath@users.noreply.github.com>
2020-09-14 08:58:24 -0600

    Update ex2.cpp

--- ex2.cpp
+++ ex2.cpp
@@ -3,7 +3,5 @@
 //print command line arguments
 int main(int argc, char* argv[]) {
     std::cout << argv[0] << std::endl;
-    if(argv[1]){
-        std::cout << argv[1] << std::endl;
-    }
+    std::cout << argv[1] << std::endl;
 }
