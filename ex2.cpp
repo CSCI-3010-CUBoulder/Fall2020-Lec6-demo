114d51fe7a76ad7d18e7ab6b9de6a08dbe8d15bd
Sreesha <sreesha.s.nath@gmail.com>
2020-09-14 08:56:45 -0600

    reverting to old file state

--- ex2.cpp
+++ ex2.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 
-//print command line arguments
+
 int main(int argc, char* argv[]) {
     std::cout << argv[0] << std::endl;
-    if(argc > 1){
-        std::cout << argv[1] << std::endl;
-    }
+    std::cout << argv[1] << std::endl;
 }
