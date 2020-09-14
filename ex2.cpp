9881d0db4dac880c8fb0f2ff3951488394f81cc4
Sreesha <sreesha.s.nath@gmail.com>
2020-09-14 08:59:18 -0600

    "checking for number of arguments"

--- ex2.cpp
+++ ex2.cpp
@@ -3,7 +3,8 @@
 //print command line arguments
 int main(int argc, char* argv[]) {
     std::cout << argv[0] << std::endl;
-    if(argv[1]){
-        std::cout << argv[1] << std::endl;
-    }
+    if (argc > 1){
+    	std::cout << argv[1] << std::endl;
+	}
 }
+    
